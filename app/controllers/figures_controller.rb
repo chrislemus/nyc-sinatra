013fac72332f9ae4cc48c0acb6f63b0e7a55dc5b
chrislemus <icrislemus@gmail.com>
2021-01-27 15:14:13 -0500

    commit tree 2cc34d6419775940967a16f4406e33b03c50519d

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,54 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get('/figures/new') {
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  }
+
+  post('/figures') {
+    figure = Figure.create(name: params[:figure][:name])
+
+    landmark_ids = params[:figure][:landmark_ids] ? params[:figure][:landmark_ids] : []
+    landmark_ids << Landmark.create(name: params[:landmark][:name], figure_id: figure.id) if !params[:landmark][:name].empty?
+
+    landmark_ids.each{ |landmark_id|
+      l = Landmark.find(landmark_id)
+      l.figure_id = figure.id
+      l.save
+    }
+
+    new_title_name = params[:title][:name]
+    title_ids = new_title_name.empty? ? [] : [Title.create(name: new_title_name).id]
+    title_ids << params[:figure][:title_ids]
+    title_ids.flatten.each{ |title_id| FigureTitle.create(title_id: title_id, figure_id: figure.id) }
+  }
+  get('/figures/:id/edit') {
+    @figure = Figure.find(params[:id])
+    @titles = @figure.titles
+    # binding.pry
+    erb :'figures/edit'
+  }
+  patch('/figures/:id') {
+    # binding.pry
+    figure = Figure.find(params[:id])
+    figure.name = params[:figure][:name]
+    new_landmark = Landmark.create(name: params[:new_landmark], figure_id: figure.id)
+    figure.save
+    redirect "/figures/#{figure.id}"
+    # @figure = Figure.find(params[:id])
+    # erb :'figures/edit'
+  }
+
+  get('/figures/:id') {
+    @figure = Figure.find(params[:id])
+    @landmarks = @figure.landmarks
+    erb :'figures/show'
+  }
+
+  get('/figures') {
+    @figures = Figure.all
+    erb :'figures/index'
+  }
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get('/landmarks/new') {
+    erb :'landmarks/new'
+  }
+
+  post('/landmarks') {
+    Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    redirect '/landmarks'
+  }
+
+  get('/landmarks') {
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  }
+  patch('/landmarks') {
+    landmark = Landmark.find(params[:landmark][:id])
+    landmark.name = params[:landmark][:name]
+    landmark.year_completed = params[:landmark][:year_completed]
+    landmark.save
+    redirect "/landmarks/#{landmark.id}"
+  }
+
+  get('/landmarks/:id/edit') {
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  }
+
+  get('/landmarks/:id') {
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  }
 end
